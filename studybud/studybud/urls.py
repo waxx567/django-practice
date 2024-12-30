b1c2df67b2ccd858d127acd701ae14d09c7a1ff3
Wayne McRae <waynem567@gmail.com>
2024-12-30 14:05:39 +0200

    Update urls.py

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -2,8 +2,9 @@
 URL Configuration for the studybud project
 '''
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
 ]
