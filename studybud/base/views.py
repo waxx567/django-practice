3824917776075cfa7c4f8e98af468aa3cdb65243
Wayne McRae <waynem567@gmail.com>
2024-12-30 15:48:01 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hello, Django!</h1>")
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'room.html')
