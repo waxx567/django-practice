551c2cd8d37a2f62f3e5e06d417de34384a6d379
Wayne McRae <waynem567@gmail.com>
2024-12-30 17:41:33 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Learn Python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
 def home(request):
     return render(request, 'home.html')
 
