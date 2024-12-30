8089ee3ac80bbd68a409b0f0859e95217844014c
Wayne McRae <waynem567@gmail.com>
2024-12-30 14:05:34 +0200

    Update settings.py

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base = studybud.base.apps.BaseConfig',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
