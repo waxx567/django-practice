9729ea3015a507804f8eaf2c189e8f8c53bacbe4
Wayne McRae <waynem567@gmail.com>
2024-12-30 18:32:03 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -13,4 +13,4 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
