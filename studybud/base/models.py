53ad38cd7025400ab3c39c711279a766eeaee553
Wayne McRae <waynem567@gmail.com>
2025-01-02 11:16:54 +0200

    Update models.py

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = models.ForeignKey(User, on_delete=models.CASCADE)
+    # topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = models.ManyToManyField(User, related_name='rooms')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
