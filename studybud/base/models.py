995b6eee77d534b5e5e66389948676a4b2d8f46d
Wayne McRae <waynem567@gmail.com>
2025-01-02 11:53:01 +0200

    Update models.py

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
 class Room(models.Model):
-    # host = models.ForeignKey(User, on_delete=models.CASCADE)
-    # topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants = models.ManyToManyField(User, related_name='rooms')
