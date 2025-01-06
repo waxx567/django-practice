1e02b06bbe297eb884840a22436e286c0ed9588e
Wayne McRae <waynem567@gmail.com>
2025-01-06 16:46:38 +0200

    Update models.py

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -11,8 +11,8 @@ class Topic(models.Model):
     
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL)
+    # host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants = models.ManyToManyField(User, related_name='rooms')
