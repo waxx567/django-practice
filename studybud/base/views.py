b3b8144959f54e466170149f66f49054dd003696
Wayne McRae <waynem567@gmail.com>
2025-01-06 17:39:45 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -18,3 +18,6 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    return render(request)
