17ff534dc30e566f97b8c4a96ea0cde1c942e448
Wayne McRae <waynem567@gmail.com>
2025-01-09 11:11:08 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -12,7 +12,8 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
-    return render(request, 'base/login_register.html')
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
