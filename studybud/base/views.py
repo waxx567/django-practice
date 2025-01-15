1c5d118620772a72740cee8a66b018318f6502c9
Wayne McRae <waynem567@gmail.com>
2025-01-15 08:12:01 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
