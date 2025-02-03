3ffd513a731c430cab0390e9c008ca3224b0c817
Wayne McRae <waynem567@gmail.com>
2025-02-03 10:05:00 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -92,6 +92,7 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        return redirect('room', pk=room.id)
 
     context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
