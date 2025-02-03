9638cbe1dedd83ec13c3807acb4e3db0d1d0c8aa
Wayne McRae <waynem567@gmail.com>
2025-02-03 10:39:13 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -85,6 +85,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created_at')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -94,7 +95,7 @@ def room(request, pk):
         )
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
