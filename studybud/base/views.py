84694736004a24df8ad4bdcfdf2b0d8e777e0fc2
Wayne McRae <waynem567@gmail.com>
2025-02-03 13:38:00 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -105,7 +105,9 @@ def room(request, pk):
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
-    context = {'user': user, 'rooms': rooms}
+    room_message = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_message': room_message, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 
