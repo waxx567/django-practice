d391efa4297694d4cc54d1c9effcc8c0e052a7d8
Wayne McRae <waynem567@gmail.com>
2025-02-03 11:24:26 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -79,7 +79,12 @@ def home(request):
     room_count = rooms.count()
     room_messages = Message.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {
+        'rooms': rooms, 
+        'topics': topics, 
+        'room_count': room_count, 
+        'room_messages': room_messages
+    }
     return render(request, 'base/home.html', context)
 
 
