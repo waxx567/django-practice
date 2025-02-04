dc22b21de83025189a69c244eea501ccd69524b6
Wayne McRae <waynem567@gmail.com>
2025-02-04 12:08:40 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -182,14 +182,6 @@ def deleteMessage(request, pk):
 
 
 @login_required(login_url='login')
-def updateUser(request):
-    user = request.user
-    form = UserCreationForm(instance=user)
-
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('home')        
+def updateUser(request):    
     
-    return render(request, 'base/update-user.html', {'form': form})
+    return render(request, 'base/update-user.html')
