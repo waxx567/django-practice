c66b7a80172917ccb59e6209914fdf887ec6527b
Wayne McRae <waynem567@gmail.com>
2025-02-05 14:16:27 +0200

    Update urls.py

--- studybud/base/api/urls.py
+++ studybud/base/api/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('routes/', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>', views.getRoom),
+]
