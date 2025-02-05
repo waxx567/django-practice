406499b0978dc604408dba460fc3cf0d686b7e60
Wayne McRae <waynem567@gmail.com>
2025-02-05 16:02:20 +0200

    Update views.py

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
 
 
 @api_view(['GET'])
@@ -13,4 +14,5 @@ def getRoutes(request):
 
 
 def getRooms(request):
+    rooms = Room.objects.all()
     return Response(rooms)
