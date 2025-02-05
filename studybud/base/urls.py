0202c9f01b17575adaf29cf210d56fb3aa1cc130
Wayne McRae <waynem567@gmail.com>
2025-02-05 12:16:51 +0200

    Update urls.py

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('update-user/', views.updateUser, name='update-user'),
 
     path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 ]
