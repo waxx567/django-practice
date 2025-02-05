9bf234fc46c5217f727c60bf015216239fff8253
Wayne McRae <waynem567@gmail.com>
2025-02-05 16:15:50 +0200

    Update serializers.py

--- studybud/base/api/serializers.py
+++ studybud/base/api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import ModelSerializer
 from base.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
