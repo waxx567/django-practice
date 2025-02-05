50ce82e5e2f15f595b6f6ede3884cdb482f86979
Wayne McRae <waynem567@gmail.com>
2025-02-05 15:33:36 +0200

    Update urls.py

--- studybud/base/api/urls.py
+++ studybud/base/api/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('routes/', views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('rooms/<str:pk>', views.getRoom),
 ]
