783e9ef75200f42728591178a4d5f714a03c164b
Wayne McRae <waynem567@gmail.com>
2025-02-07 07:50:13 +0200

    Update settings.py

--- customusermodel/customusermodel/settings.py
+++ customusermodel/customusermodel/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base.apps.BaseConfig', # Add this line to the installed apps
 ]
 
 MIDDLEWARE = [
