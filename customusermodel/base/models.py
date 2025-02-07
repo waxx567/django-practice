428b3c8b6a2214030ec5fd97c3cea933ba2449f9
Wayne McRae <waynem567@gmail.com>
2025-02-07 09:49:41 +0200

    Update models.py

--- customusermodel/base/models.py
+++ customusermodel/base/models.py
@@ -4,17 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True, unique=True)
-    username = models.CharField(max_length=200, null=True, unique=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+    
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'name']
-
-    def __str__(self):
-        return self.name
+    REQUIRED_FIELDS = []
 
