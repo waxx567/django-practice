f107ed50e679f0091b16ebfe9659f514c3c6aa97
Wayne McRae <waynem567@gmail.com>
2025-02-08 10:27:31 +0200

    Update views.py

--- buildingMVT/chefsTable/littleLemon/views.py
+++ buildingMVT/chefsTable/littleLemon/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Menu
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, Chefs Table!")
+
+def menu_by_id(request, menu_id):
+    menu = Menu.objects.get(pk=menu_id)
+    return HttpResponse(f"{menu.menu_item}: Type of {menu.cuisine} cuisine")
