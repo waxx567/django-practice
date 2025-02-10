3779c33e623032788b5a13186c04c6298c27342e
Wayne McRae <waynem567@gmail.com>
2025-02-10 18:57:18 +0200

    Update views.py

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
+
 
 def home(request):
     # return HttpResponse('Hello, World!')
     return render(request, 'webapp/index.html')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+
+    return render(request, 'webapp/register.html', context)
