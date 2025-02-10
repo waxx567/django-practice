4969326c45879b57515a7ae279ef17eaba726f1f
Wayne McRae <waynem567@gmail.com>
2025-02-10 10:53:42 +0200

    Update forms.py

--- crm/webapp/forms.py
+++ crm/webapp/forms.py
@@ -2,9 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class CreateUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "password1", "password2"]
