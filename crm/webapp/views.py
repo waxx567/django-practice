5cfc4662417425ea3f4a29d4d6bf23573f25c045
Wayne McRae <waynem567@gmail.com>
2025-02-11 09:02:08 +0200

    Update views.py

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -29,10 +29,12 @@ def my_login(request):
 
     if request.method == 'POST':
         form = LoginForm(request.POST)
+
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
+            
             if user is not None:
                 login(request, user)
                 return redirect('home')
