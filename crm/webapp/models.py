d71d0b9f0469ce7d4d8970be2cfca8fed427c2a8
Wayne McRae <waynem567@gmail.com>
2025-02-11 13:10:13 +0200

    Data Management

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=20)
