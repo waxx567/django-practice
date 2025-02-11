fd61a153ffb25e7f48c7d6204d5d875a01b6fc6d
Wayne McRae <waynem567@gmail.com>
2025-02-11 09:11:38 +0200

    Update views.py

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -33,12 +33,12 @@ def my_login(request):
         if form.is_valid():
             username = request.POST.get('username')
             password = request.POST.get('password')
-            
+
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                login(request, user)
-                return redirect('home')
+                auth.login(request, user)
+                # return redirect('')
 
     context = {'form': form}
 
