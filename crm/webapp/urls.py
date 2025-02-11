148ea2ef8b265ba010c42869a7ecdcc2ec76a42b
Wayne McRae <waynem567@gmail.com>
2025-02-11 15:53:35 +0200

    Update urls.py

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -4,12 +4,14 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name=''),
+    
 
     path('register/', views.register, name='register'),
 
     path('my-login/', views.my_login, name='my-login'),
 
     path('user-logout/', views.user_logout, name='user-logout'),
+
 
     path('dashboard/', views.dashboard, name='dashboard'),
 
