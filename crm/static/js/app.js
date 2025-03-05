e011ff52eb34298ad93fb28d083b7741de245473
Wayne McRae <waynem567@gmail.com>
2025-03-05 09:12:59 +0200

    Update app.js

--- crm/static/js/app.js
+++ crm/static/js/app.js
@@ -1,7 +1,9 @@
+// Initiate the message timeout
+
 var message_timeout = document.getElementById('message-timer');
 
 setTimeout(function() {
 
     message_timeout.style.display = 'none';
-    
+
 }, 5000);
